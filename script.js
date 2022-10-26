f0b75ae062cedacc337ab3dd1eb247e68547e4d2
Vikki <anexcruise@gmail.com>
2022-10-26 23:10:18 +0530

    added deleting text

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const h1 = document.getElementById('text')
 
-const texts = ['Hello, My name is ANEX JERIN', 'I love to code', 'Nice to see you']
+const texts = [
+  'Hello, My name is ANEX JERIN',
+  'I love to code',
+  'Nice to see you',
+]
 let j = 0
 let i = 0
 let currentText = []
+let deleting = false
 function loop() {
   h1.innerText = currentText.join('')
-  console.log(i);
+  console.log(i)
   if (i < texts.length) {
-    if (j <= texts[i].length) {
+    if (!deleting && j <= texts[i].length) {
       currentText.push(texts[i][j])
       j++
     }
-    if (j == texts[i].length) {
-      i++
+    if (deleting && j <= texts[i].length) {
+      currentText.pop(texts[i][j])
+      j--
+    }
+
+    if (j==texts[i].length){
+        deleting = true
     }
   }
   setTimeout(loop, 200)
